dc260d48cac65fec9058ff8cc9585c7bc6bfcde7
Harry <henriquecbgamer@gmail.com>
2022-01-12 14:39:51 -0300

    sexo

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -24,7 +24,7 @@ module.exports = class extends Client {
     registryCommands() {
         // temporária
         this.guilds.cache.get('654375015655145485').commands.set(this.commands)
-        this.application.commands.set(this.commands)
+        //this.application.commands.set(this.commands)
     }
 
     loadCommands(path = 'src/commands') {
